ff064963b05f49113a79f99744dcd503aa41db22
Giwi <marin.xavier@gmail.com>
2026-09-11 22:43:52 +0200

    PWA implementation

--- cookiemaster/src/main.jsx
+++ cookiemaster/src/main.jsx
@@ -4,6 +4,14 @@ import App from './App'
 import { AuthProvider } from './context/AuthContext'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch((err) => {
+      console.error('Erreur SW :', err)
+    })
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
